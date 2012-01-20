ce7142d0ee210b9d81895172500e2027fbde3bb3
ipa-jsf-mf <matthias-freier@gmx.de>
2012-01-20 10:32:46 +0100

    add data files

---
+++ ros_data_files/Input/script2.sh
@@ -0,0 +1,4 @@
+rosrun srs_object_database clientTest milkbox.txt
+rosrun srs_object_database clientTest pringles.txt
+rosrun srs_object_database clientTest salt.txt
+rosrun srs_object_database clientTest bottle.txt
